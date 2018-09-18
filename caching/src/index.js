f46601ca0389dc4a4c9c0925d40fc490fbbc97b3
webxiaoma <webxiaoma@qq.com>
2018-09-18 18:06:54 +0800

    提取公共代码

--- caching/src/index.js
+++
@@ -1,7 +0,0 @@
-const jquery = require('jQuery');
-
-export default ()=>{
-  let boolean =  $.isArray([1,0,2])
-
-  return boolean;
-}
--- webpackDevMiddleware/app/index.js
+++
@@ -1,8 +0,0 @@
-import index from '../css/index.css';
-
-
-var sub = require('./sub');
-var app = document.createElement('div');
-app.innerHTML = '<h1>Hello World h1</h1>';
-app.appendChild(sub());
-document.body.appendChild(app);
